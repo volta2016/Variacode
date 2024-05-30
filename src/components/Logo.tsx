f3cc4cfd5b9db45dcd18d64725af024bf71d3b5a
volta2016 <giovoltadesign@gmail.com>
2024-05-30 14:28:32 -0400

    feat: :lipstick: update path component

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,11 +1,7 @@
 export const Logo: React.FC = () => {
   return (
     <>
-      <img
-        src="https://lambent-florentine-70fc83.netlify.app/assets/proxy-logo.svg"
-        alt="Logo"
-        className="logo"
-      />
+      <img src="../assets/proxy-logo.svg" alt="Logo" className="logo" />
     </>
   );
 };
